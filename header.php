d08f265e162f179c37b7f53c02de76c6fd78c50f
davenpos <simmywim@hotmail.com>
2026-06-09 16:41:24 -0400

    Copied over CSS from projxon-example-theme and loaded in stylesheet

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="<?php bloginfo('charset'); ?>">
-    <?php wp_head(); ?> 
+    <?php wp_head(); ?>
   </head>
   <body>
     <header>

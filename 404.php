ae2d287f2005f4f3c7faaae64659fcc707763de4
davenpos <simmywim@hotmail.com>
2026-06-12 19:10:43 -0400

    Created 404 page

---
+++ 404.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+  <p>Oops! Looks like whatever you were looking for isn't here. Click <a href="" onclick="history.go(-1); return false;">here</a> to go back to the previous page.
+<?php get_footer(); ?>

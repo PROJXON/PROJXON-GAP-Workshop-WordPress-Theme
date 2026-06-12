78af44da4c2096f96f39f3ccb65f0787d6f83315
davenpos <simmywim@hotmail.com>
2026-06-12 17:52:32 -0400

    Added 'Posted by [author] on [date] on posts

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
       <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <div class="entry-meta">
+				<span class="byline">
+          Posted by <?php the_author(); ?> on <?php echo get_the_date(); ?>
+        </span>
+			</div>
       <div class="entry-content">
         <?php the_excerpt(); ?>
       </div>
--- single.php
+++ single.php
@@ -4,6 +4,11 @@
     the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
       <h2 class="entry-title"><?php the_title(); ?></h2>
+      <div class="entry-meta">
+				<span class="byline">
+          Posted by <?php the_author(); ?> on <?php echo get_the_date(); ?>
+        </span>
+			</div>
       <div class="entry-content">
         <?php the_content(); ?>
       </div>

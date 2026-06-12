96c69e5e359f25c8220db63481b3bb0cd29f44b3
davenpos <simmywim@hotmail.com>
2026-06-12 17:31:07 -0400

    Added 'Proudly powered by WordPress' to footer

--- footer.php
+++ footer.php
@@ -3,7 +3,13 @@
       <?php get_sidebar(); ?>
 
       <footer id="siteFooter">
-        <div class="site-info">&copy; <?php echo date("Y"); ?></div>
+        <div class="site-info">
+          <a href="<?php echo esc_url(__('https://wordpress.org/', 'projxon-gap-workshop-wordpress-theme')); ?>">
+				    <?php printf(esc_html__('Proudly powered by %s', 'projxon-example-theme'), 'WordPress'); ?>
+			    </a>
+          <br />
+          &copy; <?php echo date("Y"); ?>
+        </div>
       </footer>
       <?php wp_footer(); ?>
     </div><!-- #page -->

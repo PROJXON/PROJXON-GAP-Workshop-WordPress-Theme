24dd3ad6afa1e2fa12c7da2a1a3ae421a1401507
davenpos <simmywim@hotmail.com>
2026-06-13 18:05:28 -0400

    Added a comment near the top of single.php mentioning how you can display specific content just for a single post

--- single.php
+++ single.php
@@ -1,5 +1,8 @@
 <?php get_header();
 
+  //If you want extra content on the post with an ID of 7, uncomment out the following line. Alternatively, you can also create a new file called single-7.php or single-[The slug for that post].php.
+  //if (get_the_ID() === 7) echo '<p>This only appears on the post with an ID of 7</p>';
+
   while (have_posts()) {
     the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>

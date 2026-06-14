2c48d8c83da8713358c9a46ae020935c9d6afb4b
davenpos <simmywim@hotmail.com>
2026-06-14 16:50:00 -0400

    Installed @wordpress/scripts NPM package and added JavaScript file to theme

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6d605dc13a130784b3cd');
--- functions.php
+++ functions.php
@@ -5,6 +5,9 @@ function pjxnwpws_add_scripts() {
 
   //Import custom Google Font from external URL
   wp_enqueue_style('custom_font', '//fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Source+Serif+4:opsz,wght@8..60,600;8..60,700&display=swap');
+
+  //Import JS file from build folder
+  wp_enqueue_script('main-js', get_theme_file_uri('/build/index.js'), ['jquery'], filemtime(get_theme_file_path("/build/index.js")), true);
 }
 
 add_action('wp_enqueue_scripts', 'pjxnwpws_add_scripts');

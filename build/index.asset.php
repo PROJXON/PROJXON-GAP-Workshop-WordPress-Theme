a4513fe12f5d73ddd3c37e7449030e21032f0b58
davenpos <simmywim@hotmail.com>
2026-06-14 19:07:51 -0400

    Added HTML and CSS to power live search results

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '6d605dc13a130784b3cd');
+<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
--- searchform.php
+++ searchform.php
@@ -1,5 +1,12 @@
 <form role="search" method="GET" id="searchform" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
-  <label class="screen-reader-text"><?php echo _x('Search for:', 'label'); ?></label>
-	<input type="search" id="s" class="search-field" placeholder="<?php echo esc_attr_x('Search…', 'placeholder'); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label'); ?>" />
+	<label class="screen-reader-text"><?php echo _x('Search for:', 'label'); ?></label>
+	<div id="searchWrapper">
+		<input type="search" id="s" class="search-field" placeholder="<?php echo esc_attr_x('Search…', 'placeholder'); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label'); ?>" />
+		<div id="liveSearch">
+			<li><a href="#">Blog post by <span>Author</span></a></li>
+			<li><a href="#">Blog post by <span>Author</span></a></li>
+			<li><a href="#">Blog post by <span>Author</span></a></li>
+		</div>
+	</div><!-- #searchWrapper -->
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button'); ?>" />
 </form>

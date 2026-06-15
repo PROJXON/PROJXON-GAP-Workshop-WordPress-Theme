03ccb80b560f9d7c9ce83a9fd49cefc1243d6654
davenpos <simmywim@hotmail.com>
2026-06-15 19:40:25 -0400

    Separated entry-meta div into its own template part to keep code DRY

--- single.php
+++ single.php
@@ -7,20 +7,7 @@
     the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
       <h2 class="entry-title"><?php the_title(); ?></h2>
-      <div class="entry-meta">
-				<span class="byline">
-          Posted by
-          <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>">
-            <?php echo esc_html(get_the_author()); ?>
-          </a>
-          on
-          <a href="<?php echo esc_url(get_day_link(get_the_date('Y'), get_the_date('m'), get_the_date('d'))); ?>">
-            <?php echo esc_html(get_the_date()); ?>
-          </a>
-          in
-          <?php echo get_the_category_list(esc_html__(', ')); ?>
-        </span>
-			</div>
+      <?php get_template_part('template-parts/postmeta'); ?>
       <div class="entry-content">
         <?php the_post_thumbnail('banner-image');
         the_content(); ?>
--- template-parts/article-post.php
+++ template-parts/article-post.php
@@ -2,20 +2,7 @@
   <h2 class="entry-title">
     <a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title(); ?></a>
   </h2>
-  <div class="entry-meta">
-    <span class="byline">
-      Posted by
-      <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>">
-        <?php echo esc_html(get_the_author()); ?>
-      </a>
-      on
-      <a href="<?php echo esc_url(get_day_link(get_the_date('Y'), get_the_date('m'), get_the_date('d'))); ?>">
-        <?php echo esc_html(get_the_date()); ?>
-      </a>
-      in
-      <?php echo get_the_category_list(esc_html__(', ')); ?>
-    </span>
-  </div>
+  <?php get_template_part('template-parts/postmeta'); ?>
   <div class="entry-content <?php if (has_post_thumbnail()) echo 'has-thumbnail'; ?>">
     <?php the_post_thumbnail('small-thumbnail');
     the_excerpt(); ?>
